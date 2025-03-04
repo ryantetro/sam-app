f1ca329399753f4a26a136cc00be6e90ffe10660
Ryan <ryantetro@gmail.com>
2025-03-03 23:21:13 -0700

    Changed return message

--- hello-world/tests/unit/test-handler.mjs
+++ hello-world/tests/unit/test-handler.mjs
@@ -15,6 +15,6 @@ describe('Tests index', function () {
         let response = JSON.parse(result.body);
 
         expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello my friend");
+        expect(response.message).to.be.equal("Im using canary deployments");
     });
 });
